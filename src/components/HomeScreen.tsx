ba228c794aaba698245938479f916b2ad021f25a
atheimuz <atheimuz@naver.com>
2024-06-19 17:51:54 +0900

    Alert, Modal 추가

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Alert, Pressable} from 'react-native';
 
-const HomeScreen = () => <Text>홈</Text>;
+import ExampleModal from './example/ExampleModal';
+
+const showAlert = () =>
+  Alert.alert(
+    'Alert',
+    'alert 띄우기',
+    [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('alert cancel'),
+        style: 'cancel',
+      },
+    ],
+    {
+      cancelable: true,
+    },
+  );
+
+const HomeScreen = () => {
+  const [modalStatus, setModalStatus] = useState(false);
+
+  return (
+    <View>
+      <Pressable style={styles.box} onPress={showAlert}>
+        <Text style={styles.btnText}>Alert</Text>
+      </Pressable>
+      <ExampleModal status={modalStatus} setStatus={setModalStatus} />
+      <Pressable style={styles.box} onPress={() => setModalStatus(true)}>
+        <Text style={styles.btnText}>Modal</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
+  box: {
+    margin: 10,
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: 'gray',
+  },
+  btnText: {
+    fontWeight: 'bold',
+    color: 'white',
   },
 });
 
