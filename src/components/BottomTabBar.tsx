25fb3ed010c4f322bd6093400d44e76adbd469fb
atheimuz <atheimuz@naver.com>
2024-08-17 23:06:14 +0900

    하단 탭바 커스텀

---
+++ src/components/BottomTabBar.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faXmark, faHouse} from '@fortawesome/free-solid-svg-icons';
+
+const ICON_SCHEME = {
+  Home: faHouse,
+  Menu: faXmark,
+};
+
+const BottomTabBar = ({
+  state,
+  descriptors,
+  navigation,
+}: {
+  state: any;
+  descriptors: any;
+  navigation: any;
+}) => {
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.inner}>
+        {state.routes.map((route: any, index: number) => {
+          const {options} = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+              ? options.title
+              : route.name;
+
+          const isFocused = state.index === index;
+
+          const onPress = () => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name, route.params);
+            }
+          };
+
+          const onLongPress = () => {
+            navigation.emit({
+              type: 'tabLongPress',
+              target: route.key,
+            });
+          };
+
+          return (
+            <TouchableOpacity
+              key={route.key}
+              accessibilityRole="button"
+              accessibilityState={isFocused ? {selected: true} : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={styles.menu}>
+              <FontAwesomeIcon
+                icon={ICON_SCHEME[route.name]}
+                size={20}
+                style={{...(isFocused && styles.activeColor)}}
+              />
+              <Text
+                style={[
+                  styles.menuText,
+                  {...(isFocused && styles.activeColor)},
+                ]}>
+                {label}
+              </Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    height: 80,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    backgroundColor: '#fff',
+  },
+  inner: {
+    flexDirection: 'row',
+    paddingTop: 10,
+  },
+  menu: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  activeColor: {
+    color: '#1E90FF',
+  },
+  menuText: {
+    marginTop: 4,
+    fontSize: 12,
+  },
+});
+
+export default BottomTabBar;
--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import {Linking} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './HomeScreen';
 import SideScreen from './SideScreen';
 import Menu1Screen from './Menu1Screen';
 import Menu2Screen from './Menu2Screen';
+import BottomTabBar from './BottomTabBar';
 
 const Stack = createNativeStackNavigator();
-
+const Tab = createBottomTabNavigator();
 const config = {
   screens: {
     Search: 'search',
@@ -17,6 +19,27 @@ const config = {
     Menu: 'menu',
   },
 };
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      tabBar={props => <BottomTabBar {...props} />}>
+      <Tab.Screen
+        name="Home"
+        options={{tabBarLabel: '홈'}}
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        name="Menu"
+        options={{tabBarLabel: '메뉴'}}
+        component={Menu1Screen}
+      />
+    </Tab.Navigator>
+  );
+}
 function Navigator() {
   const linking = {
     prefixes: ['testdeep://'],
